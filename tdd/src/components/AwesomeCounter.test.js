9466447b63fe92eda161ce66bf6386d9b64d2551
fabianoGDB <doctorfabi.f@gmail.com>
2025-08-13 23:59:27 -0300

    A004: making the tests for AwesomeCOunter component

---
+++ tdd/src/components/AwesomeCounter.test.js
@@ -0,0 +1,10 @@
+const AwesomeCounter = ({ initialValue }) => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <span>{initialValue}</span>
+    </div>
+  );
+};
+
+export default AwesomeCounter;
