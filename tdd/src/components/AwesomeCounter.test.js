0731e87b56ce067301f47cdf3b8cc12c2a8ccb4f
fabianoGDB <doctorfabi.f@gmail.com>
2025-08-14 23:49:20 -0300

    A005: error fix

--- tdd/src/components/AwesomeCounter.test.js
+++ tdd/src/components/AwesomeCounter.test.js
@@ -1,10 +1,26 @@
-const AwesomeCounter = ({ initialValue }) => {
-  return (
-    <div>
-      <h1>Hello world</h1>
-      <span>{initialValue}</span>
-    </div>
-  );
-};
+import { render, screen } from "@testing-library/react";
+import AwesomeCounter from "./AwesomeCounter.test";
 
-export default AwesomeCounter;
+test("it should have a correct initial value when set to 7", () => {
+  render(<AwesomeCounter initialValue={7} />);
+  const count = screen.queryByText(7);
+  expect(count).toBeVisible();
+});
+
+test("it should have a correct initial value of 0", () => {
+  render(<AwesomeCounter />);
+  const count = screen.queryByText(0);
+  expect(count).toBeVisible();
+});
+
+test("it should increase the value correctly when add is clicked once", () => {
+  throw new Error();
+});
+
+test("it should decrease the value correctly when remove is clicked twice", () => {
+  throw new Error();
+});
+
+test("it shouldn't allow the value the inicyal value is 0 and remove is clicked twice", () => {
+  throw new Error();
+});
